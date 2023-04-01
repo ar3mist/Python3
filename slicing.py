6585fa9b576afaa3d893288dcd8da9ce09d88366
rohith <rohith3085@gmail.com>
2023-04-01 04:09:23 +0000

    python

---
+++ slicing.py
@@ -0,0 +1,9 @@
+parrot = "Norwegian Blue"
+
+
+print(parrot[0:])
+print(parrot[:])
+print(parrot[0:9])
+print(parrot[10:15])
+
+print(parrot[0:-1])
---
+++ string.py
@@ -0,0 +1,18 @@
+print("Python is easy to learn ")
+
+print("MY Name is " + "Rohith")
+
+name = input("Enter your name ")
+print(name)
+
+
+age = 19
+print(age)
+
+print(type(age))
+
+age = "SRM "
+
+print((type(age)))
+
+
