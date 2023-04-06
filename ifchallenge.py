9deef4e5e64bf010643674b3b1f7ed7f1c455939
rohith <rohith3085@gmail.com>
2023-04-06 17:14:46 +0000

    python

---
+++ ifchallenge.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if 18 <= age <=35:
+    print("Welcome to the club {}".format(name))
+else:
+    print("Sorry our club's are only for cool people ")
