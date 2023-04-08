c29447e3bed2a37bff84ae97097b28e46c058a8a
rohith <rohith3085@gmail.com>
2023-04-08 06:00:56 +0000

    list in python

---
+++ shoppinglist.py
@@ -0,0 +1,11 @@
+shopping_list = ["milk", "eggs", "apple", "banana", "chocolate", "rice"]
+
+for item in shopping_list:
+    print(item )
+
+print("_________________")
+
+for products in shopping_list:
+    if products == "banana":
+        break
+    print(products)
